dce71a63464fed04b5c13ae6123a75f99d283ba3
Cole Crow <colecrow@Kainas-MBP.fios-router.home>
2019-04-05 04:06:41 -0400

    working copy of javascript code that takes in a text file and outputs the count of vowels in every possible substring

---
+++ index.js
@@ -0,0 +1,30 @@
+const countVowels = str => Array.from(str)
+	.filter(letter => 'aeiouAEIOU'.includes(letter)).length;
+
+const readline = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+})
+
+readline.question(`Enter a filename: `, (name) => {
+	var fs = require('fs');
+	var array = fs.readFileSync(`${name}`).toString().split("\n");
+	var substrings = [];
+	readline.close();
+
+	for (var word in array) {
+		console.log(countVowels(getAllSubstrings(array[word])));
+	}
+
+	function getAllSubstrings(str) {
+		var i, j, result = [];
+
+		for (i = 0; i < str.length; i++) {
+			for (j = i + 1; j < str.length + 1; j++) {
+				result.push(str.slice(i, j));
+				result.join("");
+			}
+		}
+		return result.join('');
+	}
+});
